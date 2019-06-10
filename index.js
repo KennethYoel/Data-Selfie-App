0823678bc06da5ec116b4bba57eb4619f5cbc7e5
Kenneth Gutierrez <kenneth.joel@live.com>
2019-06-09 21:31:54 -0400

    Added POST method route

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('Listening at port 3000'));
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+// POST method route
+app.post('/api', (request, response) => {
+  console.log('I got a request!');
+  console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: 'success',
+    latitude: data.lat,
+    longitude: data.long
+  });
+});

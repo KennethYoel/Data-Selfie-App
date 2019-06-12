b14b15323e5b60890027489a7fa73859c7e9e0c6
Kenneth Gutierrez <kenneth.joel@live.com>
2019-06-12 12:54:51 -0400

    Added Nedb

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const dataStore = require('nedb');
+
 const app = express();
 app.listen(3000, () => console.log('Listening at port 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new dataStore('database.db');
+database.loadDatabase(); // Create and save database to local file named database.db.
+//database.insert({ name: 'privateGee', status: 'danger' }); // Adding json data into a database.
+
 // POST method route
 app.post('/api', (request, response) => {
   console.log('I got a request!');
-  console.log(request.body);
   const data = request.body;
+  const timeStamp = Date.now();
+  data.timeStamp = timeStamp;
+  database.insert(data);
+  console.log(database);
   response.json({
     status: 'success',
+    timeStamp: timeStamp,
     latitude: data.lat,
     longitude: data.long
   });
